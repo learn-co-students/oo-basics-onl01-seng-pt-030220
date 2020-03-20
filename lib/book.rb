20a509ab974d6f51cbbce1fb3b1ee47dbf4e0feb
Craig Storer <craigstorer@gmail.com>
2020-03-20 01:37:33 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,34 @@
+class Book
+  
+  def initialize(title)
+      @title = title  
+    
+  end
+  
+  def title
+   puts  "And Then There Were None"
+    @title
+  end
+  
+ attr_accessor :author
+ attr_accessor :genre
 
+  def turn_page=(turn)
+    
+    @turn_page = turn
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+    @turn_page 
+  end
+
+ def page_count= (num)
+   @page_count = num
+ end
+ 
+ def page_count
+   @page_count
+ end
+end
 
